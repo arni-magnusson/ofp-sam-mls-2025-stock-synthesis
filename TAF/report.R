114ead36ae9eb0eb675a35915025bac18a4901f7
Arni Magnusson <thisisarni@gmail.com>
2025-10-30 08:42:04 +1100

    Rename timeseries -> biomass

--- TAF/report.R
+++ TAF/report.R
@@ -2,7 +2,7 @@
 
 # Before: otoliths.csv (data), biology.csv, likelihoods.csv, stats.csv,
 #         summary.csv (output)
-# After:  biology.csv, growth.png, likelihoods.csv, stats.csv, timeseries.png,
+# After:  biology.csv, biomass.png, growth.png, likelihoods.csv, stats.csv,
 #         summary.csv (report)
 
 library(TAF)
@@ -16,22 +16,22 @@ otoliths <- read.taf("data/otoliths.csv")
 stats <- read.taf("output/stats.csv")
 summary <- read.taf("output/summary.csv")
 
-# Plot growth curves on top of otoliths
+# Plot biomass
+taf.png("biomass")
+plot(SB~Year, summary, ylim=c(0,25e3), type="n", xlab="Year",
+     ylab="Spawning biomass (t)", bty="n")
+abline(h=seq(0, 25e3, 5e3), col="gray", lty=3)
+box()
+lines(SB~Year, summary, lwd=3, col=5)
+dev.off()
+
+# Plot growth curve
 taf.png("growth")
 plot(NA, xlim=range(as.numeric(biology$Age)),
      ylim=lim(as.numeric(c(biology$Len, otoliths$Lbin))),
      xlab="Age (yr)", ylab="Length (cm)")
 points(Lbin~age, otoliths, pch=16, col=adjustcolor(6, 0.3))
 lines(Len~Age, biology, lwd=3)
-dev.off()
-
-# Plot time series of SB
-taf.png("timeseries")
-plot(SB~Year, summary, ylim=c(0,25e3), type="n", xlab="Year",
-     ylab="Spawning biomass (t)", bty="n")
-abline(h=seq(0, 25e3, 5e3), col="gray", lty=3)
-box()
-lines(SB~Year, summary, lwd=3, col=5)
 dev.off()
 
 # Format tables
